b10212261aae6c7fd79316436b5df1af7ab68991
Narayan Sharma <sharmanarayan1991@gmail.com>
2020-02-19 19:01:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 app.get('/', (req, res) => {
-res.send('hello world update');
+res.send('hello world update new');
 });
 app.listen(3000, () => {
 console.log('application running');
